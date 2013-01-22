10f893cb755eff3e7939299e543f970b7e39763d
Jason McCarty <jmccarty@sent.com>
2013-01-22 17:42:06 -0500

    Clean up alsa/pulse reading stuff, and remove jitter (but this cuts precision in half).

--- ppm.c
+++ ppm.c
@@ -6,14 +6,17 @@
 #include <linux/input.h>
 #include <linux/uinput.h>
 
-unsigned int rate = 192000; // soundcard sample rate in Hz
-const unsigned int sync_length = 5; // minimum length of sync pulse in ms
-int16_t threshhold = 32700; // threshhold for a high signal
+bool debug = false;
 
 typedef struct {
-  enum { INIT, LOW, HIGH } type; // type of pulse
+  size_t sync_min, sync_max; // allowed length of sync pulse in samples
+  int16_t threshhold; // threshhold to be a high pulse
+} pulse_params_t;
+
+typedef struct {
+  enum { INIT, LOW, HIGH, SYNC, INVALID } type; // type of pulse
   size_t length; // length of pulse in samples
-} pulse;
+} pulse_t;
 
 typedef struct {
   unsigned int period; // length of a full cycle in samples
@@ -25,8 +28,16 @@ typedef struct {
   size_t high_max; // maximum length of high pulse in samples
 } tx_parms;
 
+typedef struct {
+  pulse_params_t params;
+  size_t samples;
+  pulse_t pulse;
+  int16_t (*buffer)[2];
+  size_t offset;
+  snd_pcm_t *handle;
+} state_t;
 
-void init_pulse(pulse *p)
+void init_pulse(pulse_t *p)
 {
   if (p) {
     p->type = INIT;
@@ -34,18 +45,108 @@ void init_pulse(pulse *p)
   }
 }
 
-// return true if datum starts a new pulse, false otherwise
-bool datum_to_pulse(int16_t datum, int16_t threshhold, pulse *p)
+void destroy_alsa (state_t *state)
 {
-  bool complete = 0;
-  if (datum > threshhold) { // high signal
+  if (state->buffer)
+    free(state->buffer);
+
+  if (state->handle)
+    snd_pcm_close(state->handle);
+}
+
+int init_alsa (state_t *state, char *dev, unsigned int rate, unsigned int period, unsigned int sync_length, int16_t threshhold)
+{
+  assert(state);
+  int ret = 0;
+  int err;
+  snd_pcm_hw_params_t *hw_params = 0;
+
+  if ((err = snd_pcm_open (&state->handle, dev, SND_PCM_STREAM_CAPTURE, 0)) < 0) {
+    fprintf (stderr, "cannot open audio device %s (%s)\n",
+       dev,
+       snd_strerror (err));
+    goto error;
+  }
+
+  if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0) {
+    fprintf (stderr, "cannot allocate hardware parameter structure (%s)\n",
+       snd_strerror (err));
+    goto error;
+  }
+
+  if ((err = snd_pcm_hw_params_any (state->handle, hw_params)) < 0) {
+    fprintf (stderr, "cannot initialize hardware parameter structure (%s)\n",
+       snd_strerror (err));
+    goto error;
+  }
+
+  if ((err = snd_pcm_hw_params_set_access (state->handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+    fprintf (stderr, "cannot set access type (%s)\n",
+       snd_strerror (err));
+    goto error;
+  }
+
+  if ((err = snd_pcm_hw_params_set_format (state->handle, hw_params, SND_PCM_FORMAT_S16_LE)) < 0) {
+    fprintf (stderr, "cannot set sample format (%s)\n",
+       snd_strerror (err));
+    goto error;
+  }
+
+  if ((err = snd_pcm_hw_params_set_rate_near (state->handle, hw_params, &rate, 0)) < 0) {
+    fprintf (stderr, "cannot set sample rate (%s)\n",
+       snd_strerror (err));
+    goto error;
+  }
+
+  if ((err = snd_pcm_hw_params_set_channels (state->handle, hw_params, 2)) < 0) {
+    fprintf (stderr, "cannot set channel count (%s)\n",
+       snd_strerror (err));
+    goto error;
+  }
+
+  if ((err = snd_pcm_hw_params (state->handle, hw_params)) < 0) {
+    fprintf (stderr, "cannot set parameters (%s)\n",
+       snd_strerror (err));
+    goto error;
+  }
+
+  if ((err = snd_pcm_prepare (state->handle)) < 0) {
+    fprintf (stderr, "cannot prepare audio interface for use (%s)\n",
+       snd_strerror (err));
+    goto error;
+  }
+
+  state->samples = (rate*period + 999)/1000;
+  state->params.sync_min = (sync_length*rate + 999)/1000;
+  state->params.sync_max = 2*state->samples;
+  state->params.threshhold = threshhold;
+  state->buffer = malloc(state->samples*sizeof(int16_t[2])); // one period worth of buffer
+  state->offset = state->samples; // indicate that buffer contains no data
+  init_pulse(&state->pulse);
+
+  goto cleanup;
+
+  error:
+    ret = 1;
+    destroy_alsa(state);
+
+  cleanup:
+    snd_pcm_hw_params_free (hw_params);
+    return ret;
+}
+
+// return true if datum starts a new pulse, false otherwise
+bool datum_to_pulse(int16_t datum, pulse_params_t *params, pulse_t *p)
+{
+  bool complete = false;
+  if (datum > params->threshhold) { // high signal
     switch (p->type) {
       case INIT: // start of high pulse
         p->type = HIGH;
         p->length = 1;
         break;
       case LOW: // end of high pulse
-        complete = 1;
+        complete = true;
         break;
       case HIGH: // continue high pulse
         p->length++;
@@ -61,18 +162,43 @@ bool datum_to_pulse(int16_t datum, int16_t threshhold, pulse *p)
         p->length++;
         break;
       case HIGH: // end of low pulse
-        complete = 1;
+        complete = true;
         break;
+    }
+  }
+
+  if (complete) {
+    if (p->length >= params->sync_min)
+      if (p->type == LOW && p->length <= params->sync_max)
+      p->type = SYNC; // this is really a sync pulse
+    else
+      p->type = INVALID; // pulse is too long
+
+    if (debug) {
+      switch (p->type) {
+        case LOW:
+          printf("L");
+          break;
+        case HIGH:
+          printf("H");
+          break;
+        case SYNC:
+          printf("S");
+          break;
+        default:
+          printf("I");
+        }
+      printf("%zu ", p->length);
     }
   }
 
   return complete;
 }
 
-bool data_to_pulse(int16_t (*data)[2], size_t *offset, size_t samples, int16_t threshhold, pulse *p)
+bool data_to_pulse(int16_t (*data)[2], size_t *offset, size_t samples, pulse_params_t *params, pulse_t *p)
 {
   while (*offset < samples) {
-    if (datum_to_pulse(data[*offset][0], threshhold, p))
+    if (datum_to_pulse(data[*offset][0], params, p))
       return true; // datum starts a new pulse, so don't update offset
     else
       (*offset)++;
@@ -80,20 +206,20 @@ bool data_to_pulse(int16_t (*data)[2], size_t *offset, size_t samples, int16_t t
   return false;
 }
 
-void alsa_to_pulse(snd_pcm_t *capture_handle, int16_t (*data)[2], size_t *offset, size_t samples, int16_t threshhold, pulse *p)
+void read_pulse_alsa(state_t *state)
 {
   int err;
-  init_pulse(p);
+  init_pulse(&state->pulse);
   for (;;) {
-    if (*offset == samples) { // refill buffer
-      if ((err = snd_pcm_readi (capture_handle, data, samples)) != samples) {
+    if (state->offset == state->samples) { // refill buffer
+      if ((err = snd_pcm_readi (state->handle, state->buffer, state->samples)) != state->samples) {
         fprintf (stderr, "read from audio interface failed (%s)\n",
            snd_strerror (err));
         exit (1);
       }
-      *offset = 0;
+      state->offset = 0;
     }
-    if (data_to_pulse(data, offset, samples, threshhold, p))
+    if (data_to_pulse(state->buffer, &state->offset, state->samples, &state->params, &state->pulse))
       break; // found a complete pulse
   }
 }
@@ -102,118 +228,66 @@ main (int argc, char *argv[])
 {
   unsigned int rate = 1000000; // audio sample rate in Hz; will be updated by ALSA call
   unsigned int period = 20; // length of TX cycle in ms
-  size_t samples; // number of samples in a period
-  enum { ST_UNKNOWN, ST_SYNC, ST_LOW, ST_HIGH } state = ST_UNKNOWN; // type of current pulse
   int i;
   int err;
 
   /* initialize uinput joystick stuff */
   int uinput;
-  uinput = open("/dev/input/uinput", O_WRONLY | O_NONBLOCK);
-  if (uinput < 0) {
-    printf("/dev/input/uinput: %s\n", strerror(uinput));
-    exit(1);
-  }
+  if (!debug) {
+    uinput = open("/dev/input/uinput", O_WRONLY | O_NONBLOCK);
+    if (uinput < 0) {
+      fprintf(stderr, "/dev/input/uinput: %s\n", strerror(uinput));
+      exit(1);
+    }
 
-  /* we assign channels 0-6 to X, Y, Z, RX, RY, RZ */
-  err = ioctl(uinput, UI_SET_EVBIT, EV_ABS);
-  err = ioctl(uinput, UI_SET_ABSBIT, ABS_X);
-  err = ioctl(uinput, UI_SET_ABSBIT, ABS_Y);
-  err = ioctl(uinput, UI_SET_ABSBIT, ABS_Z);
-  err = ioctl(uinput, UI_SET_ABSBIT, ABS_RX);
-  err = ioctl(uinput, UI_SET_ABSBIT, ABS_RY);
-  err = ioctl(uinput, UI_SET_ABSBIT, ABS_RZ);
+    /* we assign channels 0-6 to X, Y, Z, RX, RY, RZ */
+    err = ioctl(uinput, UI_SET_EVBIT, EV_ABS);
+    err = ioctl(uinput, UI_SET_ABSBIT, ABS_X);
+    err = ioctl(uinput, UI_SET_ABSBIT, ABS_Y);
+    err = ioctl(uinput, UI_SET_ABSBIT, ABS_Z);
+    err = ioctl(uinput, UI_SET_ABSBIT, ABS_RX);
+    err = ioctl(uinput, UI_SET_ABSBIT, ABS_RY);
+    err = ioctl(uinput, UI_SET_ABSBIT, ABS_RZ);
 
-  struct uinput_user_dev uidev;
-  memset(&uidev, 0, sizeof(uidev));
-  snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "ppmjoy");
-  uidev.id.bustype = BUS_USB;
-  uidev.id.vendor = 0x1234;
-  uidev.id.product = 0xfedc;
-  uidev.id.version = 1;
-  for (i = 0; i < 6; i++) {
-    uidev.absmax[i] = 480; // set maximum values to a pulse length of 2.5ms @192kHz
+    struct uinput_user_dev uidev;
+    memset(&uidev, 0, sizeof(uidev));
+    snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "ppmjoy");
+    uidev.id.bustype = BUS_USB;
+    uidev.id.vendor = 0x1234;
+    uidev.id.product = 0xfedc;
+    uidev.id.version = 1;
+    for (i = 0; i < 6; i++) {
+      uidev.absmax[i] = 480; // set maximum values to a pulse length of 2.5ms @192kHz
+    }
+    err = write(uinput, &uidev, sizeof(uidev));
+    err = ioctl(uinput, UI_DEV_CREATE);
   }
-  err = write(uinput, &uidev, sizeof(uidev));
-  err = ioctl(uinput, UI_DEV_CREATE);
 
   /* initialize alsa stuff */
-  snd_pcm_t *capture_handle;
-  snd_pcm_hw_params_t *hw_params;
-
-  if ((err = snd_pcm_open (&capture_handle, argv[1], SND_PCM_STREAM_CAPTURE, 0)) < 0) {
-    fprintf (stderr, "cannot open audio device %s (%s)\n",
-       argv[1],
-       snd_strerror (err));
-    exit (1);
-  }
-
-  if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0) {
-    fprintf (stderr, "cannot allocate hardware parameter structure (%s)\n",
-       snd_strerror (err));
-    exit (1);
-  }
-
-  if ((err = snd_pcm_hw_params_any (capture_handle, hw_params)) < 0) {
-    fprintf (stderr, "cannot initialize hardware parameter structure (%s)\n",
-       snd_strerror (err));
-    exit (1);
-  }
-
-  if ((err = snd_pcm_hw_params_set_access (capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
-    fprintf (stderr, "cannot set access type (%s)\n",
-       snd_strerror (err));
-    exit (1);
-  }
-
-  if ((err = snd_pcm_hw_params_set_format (capture_handle, hw_params, SND_PCM_FORMAT_S16_LE)) < 0) {
-    fprintf (stderr, "cannot set sample format (%s)\n",
-       snd_strerror (err));
-    exit (1);
-  }
-
-  if ((err = snd_pcm_hw_params_set_rate_near (capture_handle, hw_params, &rate, 0)) < 0) {
-    fprintf (stderr, "cannot set sample rate (%s)\n",
-       snd_strerror (err));
-    exit (1);
-  }
-
-  if ((err = snd_pcm_hw_params_set_channels (capture_handle, hw_params, 2)) < 0) {
-    fprintf (stderr, "cannot set channel count (%s)\n",
-       snd_strerror (err));
-    exit (1);
-  }
-
-  if ((err = snd_pcm_hw_params (capture_handle, hw_params)) < 0) {
-    fprintf (stderr, "cannot set parameters (%s)\n",
-       snd_strerror (err));
-    exit (1);
-  }
-
-  snd_pcm_hw_params_free (hw_params);
-
-  if ((err = snd_pcm_prepare (capture_handle)) < 0) {
-    fprintf (stderr, "cannot prepare audio interface for use (%s)\n",
-       snd_strerror (err));
-    exit (1);
-  }
-
-  samples = (rate*period + 999)/2000; // update every half a TX period
-  int sync_length_cycles = (sync_length*rate + 999)/1000;
-  int16_t (*data)[2] = malloc(samples*sizeof(int16_t[2])); // one period worth of buffer
-  size_t offset;
-  pulse p;
+  state_t state;
+  init_alsa(&state, argv[1], 1000000, 10, 5, 32700);
 
   /* read pulses from TX and forward them to uinput */
 
   init: // look for a sync pulse
-  offset = samples;
+  if (debug)
+    printf("init\n");
+
   for (;;) {
-    alsa_to_pulse(capture_handle, data, &offset, samples, threshhold, &p);
-    if (p.type == LOW && p.length >= sync_length_cycles)
+    read_pulse_alsa(&state);
+    if (state.pulse.type == SYNC)
       break; // found a complete sync pulse
   }
 
+  // ignore initial high pulse
+  read_pulse_alsa(&state);
+  if (state.pulse.type != HIGH)
+    goto init;
+
+  if (debug)
+    printf("\n");
+
+  int prev_length[6] = {0, 0, 0, 0, 0, 0};
   for (;;) {
     for (i = 0; i < 6; i++) {
       struct input_event ev;
@@ -221,24 +295,47 @@ main (int argc, char *argv[])
       ev.type = EV_ABS;
       ev.code = ABS_X+i;
 
-      // read high pulse
-      alsa_to_pulse(capture_handle, data, &offset, samples, threshhold, &p);
-      ev.value = p.length;
+      // look for a low pulse
+      read_pulse_alsa(&state);
+      if (state.pulse.type != LOW)
+        goto init;
+      ev.value = state.pulse.length;
 
-      // read low pulse
-      alsa_to_pulse(capture_handle, data, &offset, samples, threshhold, &p);
-      ev.value += p.length;
+      // followed by a high pulse
+      read_pulse_alsa(&state);
+      if (state.pulse.type != HIGH)
+        goto init;
+      ev.value += state.pulse.length;
 
-      // send value to uinput
-      err = write(uinput, &ev, sizeof(ev));
+      // remove jitter
+      if (ev.value >= prev_length[i] - 1 && ev.value <= prev_length[i] + 1)
+        ev.value = prev_length[i];
+
+      if (debug) {
+        printf("%i ", ev.value);
+      } else {
+        // send value to uinput
+        err = write(uinput, &ev, sizeof(ev));
+      }
+
+      prev_length[i] = ev.value;
     }
 
-    // skip sync pulse, or reinitialize if there is no sync pulse
-    alsa_to_pulse(capture_handle, data, &offset, samples, threshhold, &p);
-    if (p.length < sync_length_cycles || p.length > samples)
+    // skip sync pulse and following high pulse
+    read_pulse_alsa(&state);
+    if (state.pulse.type != SYNC)
       goto init;
+
+    read_pulse_alsa(&state);
+    if (state.pulse.type != HIGH)
+      goto init;
+
+    if (debug)
+      printf("\n");
   }
 
-  snd_pcm_close (capture_handle);
+  destroy_alsa(&state);
+  ioctl(uinput, UI_DEV_DESTROY);
+  close(uinput);
   exit (0);
 }
